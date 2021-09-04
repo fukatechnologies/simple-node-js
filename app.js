e2dcba1c8b4ea20c0886073991cafb239418ae19
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2021-09-04 22:29:21 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+var port = process.env.PORT;
+if (typeof(PhusionPassenger) !== 'undefined') {
+    PhusionPassenger.configure({ autoInstall: false });
+    port = 'passenger';
+}
+
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(process.env.PORT);
+}).listen(port);
 
 console.log('Server running...');
